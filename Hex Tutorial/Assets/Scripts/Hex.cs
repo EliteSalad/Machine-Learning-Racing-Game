ddfd62692fe3d5c2b03e7101c9d7b30b4d049383
EliteSalad <xavieroliver42@gmail.com>
2019-02-12 18:26:03 +0000

    Hex Tutorial
    Hex Tutorial Co-opted to aid in map creation

---
+++ Hex Tutorial/Assets/Scripts/Hex.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hex : MonoBehaviour {
+
+    // Our coordinates in the map array
+    public bool odd = false;
+    public int rowNumber;
+	public int x;
+	public int y;
+
+    GameObject left, right, upperLeft, upperRight, lowerLeft, lowerRight;
+
+    private void Start()
+    {
+        if (x % 2 == 1)     //x represents the row number in this situation
+            odd = true;
+
+        else
+            odd = false;
+        
+    }
+
+    public Hex[] GetNeighbours() {
+
+        ///Can use neigbors to determin the order the track should be built in?
+        ///if last hex is adjacent connect the hexes else current hex connects to other adjacent hex
+		left = GameObject.Find("Hex_" + (x-1) + "_" + y);
+
+		right = GameObject.Find("Hex_" + (x+1) + "_" + y);
+
+
+        if (odd)
+        {
+            //odd = true;
+            upperLeft = GameObject.Find("Hex_" + (x) + "_" + (y + 1));
+            upperRight = GameObject.Find("Hex_" + (x + 1) + "_" + (y + 1));
+            lowerLeft = GameObject.Find("Hex_" + (x) + "_" + (y - 1));
+            lowerRight = GameObject.Find("Hex_" + (x + 1) + "_" + (y - 1));
+        }
+        else
+        {
+            //odd = false;
+            upperLeft = GameObject.Find("Hex_" + (x - 1) + "_" + (y + 1));
+            upperRight = GameObject.Find("Hex_" + (x) + "_" + (y + 1));
+            lowerLeft = GameObject.Find("Hex_" + (x - 1) + "_" + (y - 1));
+            lowerRight = GameObject.Find("Hex_" + (x) + "_" + (y - 1));
+        }
+
+
+
+
+
+        return null;
+	}
+
+}
---
+++ Hex Tutorial/Assets/Scripts/Map.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+	public GameObject hexPrefab;
+
+	// Size of the map in terms of number of hex tiles
+	// This is NOT representative of the amount of 
+	// world space that we're going to take up.
+	// (i.e. our tiles might be more or less than 1 Unity World Unit)
+	public int width = 64;
+	public int height = 40;
+
+	float xOffset = 0.882f;
+	float zOffset = 0.764f;
+
+	// Use this for initialization
+	void Start () {
+
+        CreateMap();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void CreateMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+
+                float xPos = x * xOffset;
+
+                // Are we on an odd row?
+                if (y % 2 == 1)
+                {
+                    xPos += xOffset / 2f;
+                }
+
+                GameObject hex_go = (GameObject)Instantiate(hexPrefab, new Vector3(xPos, 0, y * zOffset), Quaternion.identity);
+
+                // Name the gameobject something sensible.
+                hex_go.name = "Hex_" + x + "_" + y;
+
+                // Make sure the hex is aware of its place on the map
+                hex_go.GetComponent<Hex>().x = x;
+                hex_go.GetComponent<Hex>().y = y;
+                hex_go.GetComponent<Hex>().rowNumber = x;
+
+                // For a cleaner hierachy, parent this hex to the map
+                hex_go.transform.SetParent(this.transform);
+
+                // TODO: Quill needs to explain different optimization later...
+                hex_go.isStatic = true;
+
+            }
+        }
+    }
+}
