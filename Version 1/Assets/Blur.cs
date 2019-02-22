26f8ea884741cbf907ae27aa64ef450c456bad2e
EliteSalad <32502593+EliteSalad@users.noreply.github.com>
2019-02-22 16:07:25 +0000

    Scenes Updated
    all scenes that will be used have been created, layout has been aproximated. Other assets like text mesh pro have been added these files may need to be removed at a later date

---
+++ Version 1/Assets/Blur.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Blur : MonoBehaviour {
+
+    public Blur blurEffect;
+
+    void Update()
+    {
+        // Toggles blur effect
+        if (Input.GetKeyDown(KeyCode.Space))
+            blurEffect.enabled = !blurEffect.enabled;
+    }
+}
---
+++ Version 1/Assets/ButtonManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneMangement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public string buildCourseScene, driveScene, practiceScene;
+
+    public void Drive()
+    {
+
+    }
+
+    public void PracticeCourse()
+    {
+       // SceneManager.LoadScene();
+    }
+
+    public void CreateDriver()
+    {
+
+    }
+
+    public void BuildCourse()
+    {
+        //SceneManager.LoadScene();
+    }
+
+}
