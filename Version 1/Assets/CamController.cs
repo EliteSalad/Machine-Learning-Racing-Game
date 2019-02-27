8c42863b7951cd7fc2259c02dbc66c203f9dd8aa
EliteSalad <32502593+EliteSalad@users.noreply.github.com>
2019-02-27 12:33:16 +0000

    Cameras implemeted
    cameras can change based on enum feelers / raycast to be added next

---
+++ Version 1/Assets/CamController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour {
+
+    public enum CameraMode { POV, Follow, Top, Locked, Free, Track }
+    public CameraMode camToggler ;
+    public Transform targetCar = null;
+
+    // Use this for initialization
+    void Start () {
+        camToggler = CameraMode.Track;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (targetCar == null)
+            targetCar = GameObject.Find("/Racers/Car").transform;
+        //if we don't have a curent car to follow find one 
+        //when we find car make it the target
+        switch (camToggler)
+
+        {
+            case CameraMode.Follow:
+                //Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, targetCar.transform.localRotation, Time.deltaTime * 10);   //camera "smoothly" turns matches rotation of car
+                if (Vector3.Distance(Camera.main.transform.position, targetCar.position) > 15)
+                {
+                    Camera.main.transform.Translate(Vector3.forward * .15f);
+                }
+
+                Camera.main.transform.LookAt(targetCar.position + (Vector3.up * .5f));
+                break;
+            case CameraMode.Free:
+                Camera.main.transform.position = new Vector3(Mathf.Clamp(Camera.main.transform.position.x + Input.GetAxis("Horizontal"), -100, 100), Mathf.Clamp(Camera.main.transform.position.y + (Input.GetAxis("Mouse ScrollWheel") * -10), 5, 100), Mathf.Clamp(Camera.main.transform.position.z + Input.GetAxis("Vertical"), -100, 100));
+                break;
+            case CameraMode.Locked:
+                Camera.main.transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+                Camera.main.transform.position = new Vector3(targetCar.transform.position.x, 50, targetCar.transform.position.z);
+                break;
+            case CameraMode.POV:
+                Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, targetCar.transform.localRotation, Time.deltaTime * 10);   //camera "smoothly" turns matches rotation of car
+                Camera.main.transform.position = targetCar.transform.position + new Vector3(0,2,0) ;    //car position + 2 vertically 
+                break;
+            case CameraMode.Top:
+                Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, Quaternion.Euler(new Vector3(90, targetCar.transform.eulerAngles.y, 0)), Time.deltaTime * 10);
+                Camera.main.transform.position = new Vector3(targetCar.transform.position.x, 25, targetCar.transform.position.z);
+                break;
+            case CameraMode.Track:
+                Camera.main.transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Mouse ScrollWheel") * 10, Input.GetAxis("Vertical")));
+                Camera.main.transform.position = new Vector3(Mathf.Clamp(Camera.main.transform.position.x, -100, 100), Mathf.Clamp(Camera.main.transform.position.y, 2, 100), Mathf.Clamp(Camera.main.transform.position.z, -100, 100));
+                Camera.main.transform.LookAt(targetCar.transform);
+                break;
+
+        }
+    }
+
+    public void TogglerCamera()
+    {
+
+
+    }
+
+}
---
+++ Version 1/Assets/SensoryData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensoryData : MonoBehaviour {
+
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void Feelers()
+    {
+        /// array of inputs to map the up coming roads 
+        Vector3[] feeler = new Vector3[] 
+        {
+
+        };
+    }
+    void CurrentState()
+    {
+        //Speed
+        
+        //Downforce
+        
+        //Slip Limit
+        
+
+    }
+    void FixedAttributes()
+    {
+        //Torque
+        //Steering angle 
+        //Brake Torque
+
+    }
+}
